001a0eb6241e82a6b8a86dbe7dce6645df7bb14f
chiayhon-cell <chiayhoncell@gmail.com>
2023-09-11 17:36:45 +0800

    feat: 引入mybatis-plus，并设置IdEntity.id为雪花算法生成

--- framework-orm/src/main/java/cn/chiayhon/framework/entity/BaseEntity.java
+++ framework-orm/src/main/java/cn/chiayhon/framework/entity/BaseEntity.java
@@ -8,5 +8,6 @@ import static cn.chiayhon.framework.entity.Constant.DEFAULT_SERIAL_VERSION_ID;
  * 定义实体对象的基础类，继承自{@link IdEntity}
  */
 public abstract class BaseEntity extends IdEntity implements Serializable {
+
     private static final long serialVersionUID = DEFAULT_SERIAL_VERSION_ID;
 }
--- framework-orm/src/main/java/cn/chiayhon/framework/entity/IdEntity.java
+++ framework-orm/src/main/java/cn/chiayhon/framework/entity/IdEntity.java
@@ -1,26 +1,26 @@
 package cn.chiayhon.framework.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
 
 /**
  * 统一定义id的Entity基础类
  */
+@Getter
+@Setter
 public abstract class IdEntity implements Serializable {
 
     private static final long serialVersionUID = Constant.DEFAULT_SERIAL_VERSION_ID;
 
     /**
-     * 主键id
+     * 主键id。雪花算法实现。
      */
-
+    @TableId(type= IdType.ASSIGN_ID)
     protected Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
