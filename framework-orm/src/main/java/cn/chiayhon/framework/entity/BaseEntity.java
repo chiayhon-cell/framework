e029cdf5754b2950a1c590efcf933dc56ee908be
chiayhon-cell <chiayhoncell@gmail.com>
2023-09-12 09:55:21 +0800

    feat: BaseEntity去掉abstract修饰符

--- framework-orm/src/main/java/cn/chiayhon/framework/entity/BaseEntity.java
+++ framework-orm/src/main/java/cn/chiayhon/framework/entity/BaseEntity.java
@@ -7,7 +7,14 @@ import static cn.chiayhon.framework.entity.Constant.DEFAULT_SERIAL_VERSION_ID;
 /**
  * 定义实体对象的基础类，继承自{@link IdEntity}
  */
-public abstract class BaseEntity extends IdEntity implements Serializable {
+public class BaseEntity extends IdEntity implements Serializable {
 
     private static final long serialVersionUID = DEFAULT_SERIAL_VERSION_ID;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                '}';
+    }
 }
